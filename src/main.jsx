5955cfd3c3a5502922daf2f56b70292c129583bd
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-03-07 07:02:01 +0000

    progress bar css added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AudioPlayer from './components/AudioPlayer';
 import './styles/index.css';
+import './styles/customize-progress-bar.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
